d7e98e1a2b34fbb6af1a18020c1e1203b6aabe24
Priyan <113763087+priyan2906@users.noreply.github.com>
2024-04-17 12:44:50 +0530

    Add files via upload

---
+++ coke.js
@@ -0,0 +1,14 @@
+class coke{
+    waterBottle(){
+        return "kinley"
+    }
+    juice(){
+        return "mazza"
+    }
+    drink(){
+        return"coca cola"
+    }
+}
+const drinkA=new coke();
+console.log(drinkA.drink());
+
